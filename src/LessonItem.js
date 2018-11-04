d1e626bc034ea0a9376f89ef8bfed516d817c730
Cole Vanacore <colevanacore@gmail.com>
2018-11-04 13:18:38 -0700

    Toggle show lesson info

---
+++ src/LessonItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class LessonItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showLessonInfo: false
+    }
+  } 
+
+  showLessonInfo = (event) => {
+    event.preventDefault();
+    if (this.state.showLessonInfo) {
+      this.setState({
+      showLessonInfo: false
+      })
+    } else {
+      this.setState({
+      showLessonInfo: true
+    })
+    }
+  }
+
+  render() {
+    if (this.state.showLessonInfo) {
+      return (
+      <div> 
+      <h2> {this.props.lessonTitle} </h2>
+      <p> {this.props.lessonInfo} </p>
+        <button onClick={this.showLessonInfo}> show less </button>
+        </div>
+        )
+    } else {
+      return  (
+      <div> 
+      <h2> {this.props.lessonTitle} </h2>
+        <button onClick={this.showLessonInfo}> show more </button>
+      </div>
+      );
+    }
+  }
+}
+
+
+
+export default LessonItem;
